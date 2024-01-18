4c97cf2283b8b4d3a3ec85c0a33108142532a0ad
GuidoHermann <157026603+GuidoHermann@users.noreply.github.com>
2024-01-18 11:11:59 -0300

    Add files via upload

---
+++ Curso C# Nivel 1/Unidad2/Unidad2Ejercicio4/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Unidad2Ejercicio4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Una casa de computación paga a sus empleados un sueldo fijo de ARS15000 
+            //más una comisión del 5% sobre el total facturado por cada empleado.
+            // Hacer un programa para ingresar el total facturado por un empleado y
+            // que luego calcule y emita por pantalla el sueldo total a cobrar por el mismo.
+            // tengo que calcular la comision sobre el total facturado
+
+
+            float ComisionFinal;
+            float SueldoFijo;
+            float TotalFacturado;
+            float Comision;
+            float SueldoFinal;
+            Comision = 1.05F;
+            SueldoFijo = 15000;
+
+
+            Console.WriteLine("Ingresar total facturado por empleado: ");
+            TotalFacturado = float.Parse(Console.ReadLine());
+            ComisionFinal = TotalFacturado * Comision - TotalFacturado;
+            SueldoFinal = SueldoFijo + ComisionFinal;
+            Console.WriteLine(ComisionFinal);
+            Console.WriteLine("Tu sueldo de este mes es: " + SueldoFinal +" ARS");
+            
+
+
+        }
+    }
+}
---
+++ Curso C# Nivel 1/Unidad2/Unidad2Ejercicio5/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Unidad2Ejercicio5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float nota1;
+            float nota2;
+            float nota3;
+            float promedio;
+
+            Console.WriteLine("Ingrese la nota del Primer Parcial: ");
+            nota1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese la nota del Segundo Parcial: ");
+            nota2 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese la nota del Tercer Parcial: ");
+            nota3 = float.Parse(Console.ReadLine());
+
+            promedio = (nota1 + nota2 + nota3) / 3;
+
+            Console.WriteLine("Tu promedio es de: " + promedio);
+
+        }
+    }
+}
