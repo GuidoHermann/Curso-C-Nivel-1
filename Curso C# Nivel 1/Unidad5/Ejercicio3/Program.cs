7a845d9dff8ad33ee8538ac98a589f51f38b8c1b
GuidoHermann <157026603+GuidoHermann@users.noreply.github.com>
2024-01-18 11:19:53 -0300

    Add files via upload

---
+++ Curso C# Nivel 1/Unidad5/Ejercicio3/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Ejercicio3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Hacer un programa que solicite 20 edades y
+            // luego calcule el promedio de edad de aquellas personas mayores a 18 años.
+
+            int edad = 0, mayor = 0, contador = 0, promedio;
+
+            for (int x = 0; x < 20; x++)
+            {
+                Console.WriteLine("Ingrese una edad: ");
+                edad = int.Parse(Console.ReadLine());
+
+                if (edad > 18)
+                {
+                    mayor+= edad;
+                    contador++;
+
+                }
+
+            }
+            promedio = mayor / contador;
+
+            Console.WriteLine("El promedio es: " + promedio);
+
+            
+
+        }
+    }
+}
