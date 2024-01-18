aeceff2f96153919dafb45602b798fe317ced8fe
GuidoHermann <157026603+GuidoHermann@users.noreply.github.com>
2024-01-18 11:18:19 -0300

    Add files via upload

---
+++ Curso C# Nivel 1/Unidad4/Ejercicio4/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Ejercicio4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Hacer un programa para ingresar tres números y emitir un cartel aclaratorio
+            // si la suma de los dos primeros es mayor al producto del segundo con el tercero.
+
+            int A, B, C, suma, producto;
+
+            Console.WriteLine("Ingresa un nro");
+            A = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese otro nro");
+            B = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese otro nro");
+            C = int.Parse(Console.ReadLine());
+
+            suma = A + B;
+            producto = B * C;
+
+            if (suma > producto)
+            {
+                Console.WriteLine("Es mayor");
+            }else
+            {
+                Console.WriteLine("No es mayor");
+            }
+            
+            
+
+        }
+    }
+}
---
+++ Curso C# Nivel 1/Unidad4/Ejercicio5/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Ejercicio5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Hacer un programa para ingresar 4 números. Luego analizar e informar por pantalla
+            // si los mismos se encuentran ordenados de forma decreciente .
+
+            int A, B, C, D, E;
+
+            Console.WriteLine("Ingrese un nro");
+            A = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese otro nro");
+            B = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese otro nro");
+            C = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese otro nro");
+            D = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese otro nro");
+            E = int.Parse(Console.ReadLine());
+
+            if(A > B && B > C && C > D && D > E){
+                Console.WriteLine("Estan ordanos de forma decreciente");
+            }else{
+                Console.WriteLine("No estan ordenados");
+            }
+
+
+            
+            
+
+        }
+    }
+}
