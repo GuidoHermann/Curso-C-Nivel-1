c06f4938c34d10e183cc32f29f0c907048afc31c
GuidoHermann <157026603+GuidoHermann@users.noreply.github.com>
2024-01-18 11:26:37 -0300

    Add files via upload

---
+++ Curso C# Nivel 1/Unidad7/Ejercicio3.5/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Ejercicio3._5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string frase;
+            char letraActual;
+            char letraNueva;
+
+            Console.WriteLine("Ingrese una frase: ");
+            frase = Console.ReadLine();
+            Console.WriteLine("Ingrese la letra a reemplazar");
+            letraActual = char.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese la letra nueva");
+            letraNueva = char.Parse(Console.ReadLine());
+
+            frase = frase.Replace(letraActual, letraNueva);
+
+            Console.WriteLine("La cadena nueva es: ");
+            Console.Write(frase);
+        }
+    }
+}
